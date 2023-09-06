15a0a68c745952747b31dc8bdfa6f5fe06c5ea85
ketan-bhambure-bbi <141905265+ketan-bhambure-bbi@users.noreply.github.com>
2023-09-06 10:33:27 +0530

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,11 @@
+let massMark=78;
+let heightMark=1.69;
+let massJohn=92;
+let heightJohn=1.95;
+
+let BMIMark=massMark/(heightMark* heightMark);
+let BMIJohn=massJohn/(heightJohn*heightJohn);
+let markHigherBMI=BMIMark>BMIJohn;
+
+console.log(BMIMark+ BMIJohn+markHigherBMI);
+
---
+++ Assignment3.js
@@ -0,0 +1,12 @@
+const scoreDolphins =(96+108+89)/3;
+const scoreKoalas =(88+91+110)/3;
+
+if(scoreKoalas<scoreDolphins){
+    console.log("Dolphins win the trophy")
+}
+ else if(scoreDolphins<scoreKoalas){
+     console.log("Koalas win the trophy") 
+ }
+ else{
+      console.log("Both win the trophy")
+ }
