2576b14f04ceddaad0c68d79e1cc3b349855b65a
ketan-bhambure-bbi <141905265+ketan-bhambure-bbi@users.noreply.github.com>
2023-09-07 17:18:28 +0530

    Update Assignment5.js

--- Assignment5.js
+++ Assignment5.js
@@ -2,10 +2,10 @@ const calcAverage=(a,b,c)=> {return (a+b+c)/3;}
 const scoreDolphins=calcAverage(44,23,71);
 const scoreKoalas=calcAverage(85,54,71);
 function checkWinner(avgDolphins,avgKoalas){
-    if(avgDolphins>avgKoalas){
+    if(avgDolphins>2*avgKoalas){
         console.log(`Dolphin wins (${avgDolphins} .vs ${avgKoalas})`);
     }
-    else if(avgDolphins<avgKoalas){
+    else if(2*avgDolphins<avgKoalas){
         console.log(`Koalas wins (${avgKoalas} .vs ${avgDolphins})`);
     }
     else{
